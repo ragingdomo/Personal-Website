fca75e8400e0e122d5322c32f07234d093bb4634
Jonas Kulzer <kulzerjonas@gmail.com>
2023-04-16 02:44:03 +0000

    Converted Skills and About to proper flex box rules, added more rows to message box.

---
+++ src/App/About/Me/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+function Me() {
+  return (
+    <div className="me-container">
+      <h1>About Me</h1>
+      <p className="me-text">
+        Bla Bla Bla hee hoo Bla Bla Bla hee hooBla Bla Bla hee hooBla Bla Bla
+        hee hoo{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Me;
