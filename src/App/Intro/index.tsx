3d0b3e4005137472925bf322acfb82a031bcbf64
Jonas <kulzerjonas@gmail.com>
2023-04-16 19:13:28 -0500

    Made the site mobile friendly

--- src/App/Intro/index.tsx
+++ src/App/Intro/index.tsx
@@ -6,18 +6,20 @@ function Intro() {
     <div className="intro-container">
       <h1 id="name">Jonas Kulzer</h1>
       <h2>Full Stack Developer</h2>
-      <a className="nav-buttons" href="#skills">
-        Skills
-      </a>
-      <a className="nav-buttons" href="#projects">
-        Projects
-      </a>
-      <a className="nav-buttons" href="#about">
-        About Me
-      </a>
-      <a className="nav-buttons" href="#contact">
-        Contact
-      </a>
+      <div className="nav-buttons-container">
+        <a className="nav-buttons" href="#skills">
+          Skills
+        </a>
+        <a className="nav-buttons" href="#projects">
+          Projects
+        </a>
+        <a className="nav-buttons" href="#about">
+          About Me
+        </a>
+        <a className="nav-buttons" href="#contact">
+          Contact
+        </a>
+      </div>
     </div>
   );
 }
